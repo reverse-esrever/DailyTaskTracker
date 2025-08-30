035e29e7b6ad0190d655af9ac5f3d3caa905ac8b
revers-esrever <cry3far3@mail.ru>
2025-08-30 13:40:07 +0500

    Создан контроллер и маршрутизация для Category

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -13,3 +14,12 @@ Route::get('dashboard', function () {
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
+
+Route::prefix('/categories')->group(function(){
+    Route::get("/", [CategoryController::class, 'index'])->name('category.index');
+    Route::get("/create", [CategoryController::class, 'create'])->name('category.create');
+    Route::post("/", [CategoryController::class, 'store'])->name('category.store');
+    Route::get("/{category}/edit", [CategoryController::class, 'edit'])->name('category.edit');
+    Route::put("/{category}", [CategoryController::class, 'update'])->name('category.update');
+    Route::delete("/{category}", [CategoryController::class, 'destroy'])->name('category.destroy');
+});
