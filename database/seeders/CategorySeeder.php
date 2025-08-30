55af7e1ac8269fec693ded57d0ed329a368b1915
revers-esrever <cry3far3@mail.ru>
2025-08-30 13:04:26 +0500

    Добавлен CategorySeeder, CategoryFactory для заполнения бд

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use Database\Factories\CategoryFactory;
+use Database\Factories\UserFactory;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Category::factory(20)->create();
+    }
+}
